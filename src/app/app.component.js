72730e156bc45b892f74d059639bc35b1b3cd1a4
Aleksander Patschek <Olek@MacBook-Pro-Aleksander.local>
2017-03-03 22:09:49 +0100

    initital commit

---
+++ src/app/app.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+//styles
+import './app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div> starting view</div>
+    );
+  }
+}
+
+export default App;
